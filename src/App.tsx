eca3555c7588dbd24c61a46673f5594792822e8c
puneethprabhu <puneethprabhuk@gmail.com>
2025-08-23 11:29:41 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import { Button } from "./components/ui/button"
+
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Button>Submit</Button>
+    </>
+  )
+}
